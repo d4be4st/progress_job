c042dd7273cec0abc8fb3820cf5c253d82bffdcb
Stjepan Hadjic <d4be4st@gmail.com>
2014-09-22 11:08:04 +0200

    fix migration

--- lib/generators/progress_job/templates/migration.rb
+++ lib/generators/progress_job/templates/migration.rb
@@ -2,8 +2,8 @@ class AddProgressToDelayedJobs < ActiveRecord::Migration
   def change
     change_table :delayed_jobs do |t|
       t.string :progress_stage, required: true
-      t.string :progress_current, required: true, default: 0
-      t.string :progress_max, required: true, default: 0
+      t.integer :progress_current, required: true, default: 0
+      t.integer :progress_max, required: true, default: 0
     end
 
   end
