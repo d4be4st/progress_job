e72b2a41cae852b623babc0f61e183a0e4c03980
Stjepan Hadjic <d4be4st@gmail.com>
2014-09-22 11:27:14 +0200

    fix contreoller

--- app/controllers/progress_job/progress_controller.rb
+++ app/controllers/progress_job/progress_controller.rb
@@ -1,13 +1,10 @@
 module ProgressJob
   class ProgressController < ActionController::Base
 
-    respond_to :json
-
     def show
-      @current_job = Delayed::Job.find(params[:job_id])
-      percentage = @current_job.current_progress / @current_job.max_progress.to_f * 100
-      @current_job[:percentage] = percentage
-      render json: @current_job
+      @delayed_job = Delayed::Job.find(params[:job_id])
+      percentage = !@delayed_job.progress_max.zero? ? @delayed_job.progress_current / @delayed_job.progress_max.to_f * 100 : 0
+      render json: @delayed_job.attributes.merge!(percentage: percentage).to_json
     end
 
   end
