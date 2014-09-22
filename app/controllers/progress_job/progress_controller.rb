880ab826a65808aaa615a41eb3e558f3b129f37d
Stjepan Hadjic <d4be4st@gmail.com>
2014-09-22 11:13:26 +0200

    fix controller

--- app/controllers/progress_job/progress_controller.rb
+++ app/controllers/progress_job/progress_controller.rb
@@ -5,7 +5,7 @@ module ProgressJob
 
     def show
       @current_job = Delayed::Job.find(params[:job_id])
-      percentage = current_job.current_progress / current_job.max_progress.to_f * 100
+      percentage = @current_job.current_progress / @current_job.max_progress.to_f * 100
       @current_job[:percentage] = percentage
       render json: @current_job
     end
