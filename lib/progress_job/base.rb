d8ad0966f0b1aa37deda9777661b93e53455b4b4
Stjepan Hadjic <d4be4st@gmail.com>
2014-09-22 10:57:37 +0200

    changed names of colums

--- lib/progress_job/base.rb
+++ lib/progress_job/base.rb
@@ -1,17 +1,17 @@
 module ProgressJob
   class Base
-    def initialize(max_progress)
-      @max_progress = max_progress
+    def initialize(progress_max)
+      @progress_max = progress_max
       super()
     end
 
     def before(job)
       @job = job
-      @job.update_column(:max_progress, @max_progress)
+      @job.update_column(:progress_max, @progress_max)
     end
 
     def update_progress(step: 1)
-      @job.update_column(:current_progress, @job.current_progress + step)
+      @job.update_column(:progress_current, @job.progress_current + step)
     end
   end
 end
--- lib/progress_job/version.rb
+++ lib/progress_job/version.rb
@@ -1,3 +1,3 @@
 module ProgressJob
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
