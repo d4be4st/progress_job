1ec45736c30595fb20da69814a65e60d157c17e1
Stjepan Hadjic <d4be4st@gmail.com>
2014-10-13 13:24:58 +0200

    features added

--- lib/progress_job/base.rb
+++ lib/progress_job/base.rb
@@ -7,7 +7,8 @@ module ProgressJob
 
     def before(job)
       @job = job
-      @job.update_column(:progress_max, @progress_max)
+      job.update_column(:progress_max, @progress_max)
+      job.update_column(:progress_current, 0)
     end
 
     def update_progress(step: 1)
@@ -19,9 +20,12 @@ module ProgressJob
     end
 
     def update_stage_progress(stage, step: 1)
-      binding.pry
       update_stage(stage)
       update_progress(step: step)
+    end
+
+    def error(job, exception)
+      job.update_column(:progress_stage, exception.message)
     end
   end
 end
